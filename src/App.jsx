30fc31bd91635a0ee94f5338bbe63c3951aed55e
Shahab Uddin Ghori <shahabnisar918@gmail.com>
2024-08-13 21:32:03 +0500

    Add remaining changes and fix issues

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 import "./responsive.css";
 import Header from "./header";
 import MainSec from "./mainSec";
-import Footer from "./img/footer";
+import Footer from "./footer";
 import React from "react";
 import Type1js from "./type1js";
 import Type2 from "./type2js";
@@ -13,10 +13,10 @@ const App = () => {
   return (
     <>
       <Header />
+      <Type1js />
       <Type2 />
       <Type3 />
       <MainSec />
-      <Type1js />
       <Footer />
     </>
   );
---
+++ src/footer.jsx
@@ -0,0 +1,26 @@
+const Footer = () => {
+  return (
+    <footer>
+      <div className="footer-container">
+        <div className="footer-content">
+          <p>© 2024 GitHub, Inc.</p>
+          <p>
+            <a href="">Terms</a>
+          </p>
+          <p>
+            <a href="">Privacy</a>
+          </p>
+        </div>
+        <div className="footer-socialiner-link-container">
+          <i className="bx bxl-twitter"></i>
+          <i className="bx bxl-facebook-square"></i>
+          <i className="bx bxl-youtube"></i>
+          <i className="bx bxl-linkedin"></i>
+          <i className="bx bxl-github"></i>
+        </div>
+      </div>
+    </footer>
+  );
+};
+Footer();
+export default Footer;
